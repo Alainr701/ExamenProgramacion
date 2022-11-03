790751dbd948f52abc3fcfabd77868ca4256b379
a701 <a701e3127@gmail.com>
2022-11-03 10:09:08 -0400

    Examens

---
+++ src/main/Instrumentos.java
@@ -0,0 +1,33 @@
+package main;
+
+class  ClasePrincipal{
+    Instrumentos instrumentos = new Guitarra();
+    Instrumentos instrumentos2 = new Saxofón();
+    Instrumentos instrumentos3 = new Piano();
+}
+public abstract class   Instrumentos {
+     public abstract void sonido();
+}
+
+ class Guitarra extends  Instrumentos{
+     @Override
+     public void sonido() {
+         System.out.println("Guitarra");
+     }
+ }
+
+ class Saxofón extends  Instrumentos{
+     @Override
+     public void sonido() {
+         System.out.println("Saxofon");
+
+     }
+ }
+
+ class Piano extends  Instrumentos{
+     @Override
+     public void sonido() {
+         System.out.println("Piano");
+
+     }
+ }
